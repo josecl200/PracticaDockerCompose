07cd7e01fa440217adddd3457f3ae8fe9bdfa549
José C. Ureña <20160138@ce.pucmm.edu.do>
2020-08-03 17:02:20 -0400

    hazelcast

--- src/main/java/edu/pucmm/josecl200/encuestacompose/EncuestacomposeApplication.java
+++ src/main/java/edu/pucmm/josecl200/encuestacompose/EncuestacomposeApplication.java
@@ -1,10 +1,15 @@
 package edu.pucmm.josecl200.encuestacompose;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.web.WebFilter;
 import edu.pucmm.josecl200.encuestacompose.servicios.UsuarioServicios;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class EncuestacomposeApplication {
@@ -12,5 +17,19 @@ public class EncuestacomposeApplication {
         ApplicationContext applicationContext = SpringApplication.run(EncuestacomposeApplication.class, args);
         UsuarioServicios usuarioServicios = (UsuarioServicios) applicationContext.getBean("usuarioServicios");
         usuarioServicios.crearUsuarioAdmin();
+    }
+    @Bean
+    public Config config() {
+        return new Config();
+    }
+
+    @Bean
+    public WebFilter webFilter(HazelcastInstance hazelcastInstance) {
+
+        Properties properties = new Properties();
+        properties.put("instance-name", hazelcastInstance.getName());
+        properties.put("sticky-session", "false");
+
+        return new WebFilter(properties);
     }
 }
